8f40a45516dcfc45023d3452d96b3a39c7acde3a
Ashley Nelson <amn@fantashley.com>
2020-04-11 11:35:03 -0500

    Add unit test for scrabble board initialization

---
+++ pkg/scrabbleserver/board_test.go
@@ -0,0 +1,28 @@
+package scrabbleserver
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestBoard(t *testing.T) {
+	occurrences := map[string]int{
+		"star":         1,
+		"doubleLetter": 24,
+		"doubleWord":   16,
+		"tripleLetter": 12,
+		"tripleWord":   8,
+		"plain":        164,
+	}
+
+	count := make(map[string]int)
+	for _, row := range initializedBoard {
+		for _, square := range row {
+			count[square.SquareType]++
+		}
+	}
+
+	if !reflect.DeepEqual(occurrences, count) {
+		t.Error("Scrabble board does not have correct square counts")
+	}
+}
